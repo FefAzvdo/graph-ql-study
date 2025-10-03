602ed59a1f37de8012c8643b78a3947cf70dd06b
FefAzvdo <wfernandoaugusto01@gmail.com>
2025-10-03 16:37:24 -0300

    gambi

--- src/schema/typeDefs/userDef.ts
+++ src/schema/typeDefs/userDef.ts
@@ -1,19 +1,6 @@
 import { gql } from "apollo-server";
-import { readFileSync } from "fs";
-import path from "path";
 
-// lê o schema de user.graphql
-const userSchema = readFileSync(
-  path.join(import.meta.dirname, "user.graphql"),
-  "utf8"
-);
-
-export const typeDefs = gql`
-  type Query
-  type Mutation
-  # ${userSchema}
-
-
+export const userTypeDef = gql`
   type User {
     id: ID!
     nome: String!
